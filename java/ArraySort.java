6bf68d9118c0885945decbb65af3126de7632e88
Will Faught <will.faught@gmail.com>
2012-07-31 17:18:44 -0700

    ArraySort.java: Impl insertion. ListSort.java: Stub for linked list sort algs.

--- java/ArraySort.java
+++ java/ArraySort.java
@@ -4,10 +4,10 @@ public class ArraySort
 {
 	// Not stable
 	// Not adaptive
-	// Size complexity: O(1)
-	// Best time complexity: O(n^2) swaps, comparisons
-	// Average time complexity: O(n^2) swaps, comparisons
-	// Worst time complexity: O(n^2) swaps, comparisons
+	// Space complexity: O(1)
+	// Best time complexity: O(n) swaps, O(n^2) comparisons
+	// Average time complexity: O(n) swaps, O(n^2) comparisons
+	// Worst time complexity: O(n) swaps, O(n^2) comparisons
 	public static <T extends Comparable<T>> void selection(List<T> items)
 	{
 		int size = items.size();
@@ -33,9 +33,33 @@ public class ArraySort
 		}
 	}
 	
+	// Stable
+	// Adaptive
+	// Space complexity: O(1)
+	// Best time complexity: O(1) swaps, O(n) comparisons
+	// Average time complexity: O(n^2) swaps, comparisons
+	// Worst time complexity: O(n^2) swaps, comparisons
 	public static <T extends Comparable<T>> void insertion(List<T> items)
 	{
-		
+		int size = items.size();
+		for (int i = 1; i < size; ++i)
+		{
+			for (int j = i; j > 0; --j)
+			{
+				T right = items.get(j);
+				T left = items.get(j - 1);
+				if (left.compareTo(right) > 0)
+				{
+					T item = right;
+					items.set(j, left);
+					items.set(j - 1, item);
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
 	}
 	
 	public static <T extends Comparable<T>> void bubble(List<T> items)
---
+++ java/ListSort.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class ListSort
+{
+	public static <T extends Comparable<T>> void selection(List<T> items)
+	{
+	}
+	
+	public static <T extends Comparable<T>> void insertion(List<T> items)
+	{
+	}
+}
