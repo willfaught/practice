e7f656bba4a78d6b6f0c33e15809a723c74c62a2
Will Faught <will.faught@gmail.com>
2012-08-28 04:42:28 -0700

    Added a package. Changed Enumerator to Iterator.

--- java/SearchTree.java
+++ java/SearchTree.java
@@ -1,3 +1,7 @@
+package com.willfaught;
+
+import java.util.Iterator;
+
 public interface SearchTree<K extends Comparable<K>, V> extends Collection<K>
 {
     void add(K key, V value);
@@ -6,9 +10,9 @@ public interface SearchTree<K extends Comparable<K>, V> extends Collection<K>
     V get(K key);
     K getMaximum();
     K getMinimum();
-    Enumerator<K> inorder(K low, K high);
-    Enumerator<K> preorder(K low, K high);
-    Enumerator<K> postorder(K low, K high);
+    Iterator<K> inorder(K low, K high);
+    Iterator<K> preorder(K low, K high);
+    Iterator<K> postorder(K low, K high);
     int rank(K key);
     void remove(K key);
     void removeMaximum();
