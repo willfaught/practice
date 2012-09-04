528c78dcc9b3833ee1ca80518586d273981e245c
Will Faught <will.faught@gmail.com>
2012-09-03 17:21:51 -0700

    Formatted code. Changed to extend Collection. Added add(E)/remove(E) methods.

--- java/List.java
+++ java/List.java
@@ -1,10 +1,18 @@
 package com.willfaught;
 
-public interface List<E> extends Set<E>
+public interface List<E> extends Collection<E>
 {
-	void add(int index, E element);
-	E get(int index);
-	int index(E element);
-	E remove(int index);
-	E set(int index, E element);
+    void add(E element);
+
+    void add(int index, E element);
+
+    E get(int index);
+
+    int index(E element);
+
+    void remove(E element);
+
+    E remove(int index);
+
+    E set(int index, E element);
 }
