d0156abff1e49e6749b5c24f4ae1165a8eac6a95
Will Faught <will.faught@gmail.com>
2012-09-04 20:13:09 -0700

    Reordered methods.

--- java/SearchTree.java
+++ java/SearchTree.java
@@ -2,20 +2,33 @@ package com.willfaught;
 
 import java.util.Iterator;
 
-public interface SearchTree<K extends Comparable<K>, V> extends Collection<K>
+public interface SearchTree<K extends Comparable<K>, V> extends Collection<K>, Iterable<K>
 {
     void add(K key, V value);
+
     K ceiling(K key);
+
     K floor(K key);
+
     V get(K key);
+
     K getMaximum();
+
     K getMinimum();
+
     Iterator<K> inorder(K low, K high);
-    Iterator<K> preorder(K low, K high);
+
     Iterator<K> postorder(K low, K high);
+
+    Iterator<K> preorder(K low, K high);
+
     int rank(K key);
+
     void remove(K key);
+
     void removeMaximum();
+
     void removeMinimum();
+
     K select(int rank);
 }
