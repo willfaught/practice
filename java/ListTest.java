20f43df6e138f2888df1a6a5261e3ab5647bebb7
Will Faught <will.faught@gmail.com>
2012-09-17 18:02:47 -0700

    Renamed LinkedList to LinearDoublyLinkedList. Added LinearSinglyLinkedList test. Added TODO for a test. Added an add null test. Substituted assertExpected, assertEqual, and assertNotEqual where necessary.

--- java/ListTest.java
+++ java/ListTest.java
@@ -11,11 +11,18 @@ public class ListTest
     }
     
     @Test
-    public void linkedList()
+    public void linearDoublyLinkedList()
     {
-        list(new LinkedList<Integer>(), new LinkedList<Integer>());
+        list(new LinearDoublyLinkedList<Integer>(), new LinearDoublyLinkedList<Integer>());
     }
     
+    @Test
+    public void linearSinglyLinkedList()
+    {
+        list(new LinearSinglyLinkedList<Integer>(), new LinearSinglyLinkedList<Integer>());
+    }
+    
+    // TODO: remove(E) should only remove one element and return index or -1
     private static void list(List<Integer> list1, List<Integer> list2)
     {
         begin("init");
@@ -30,6 +37,8 @@ public class ListTest
         end();
         
         begin("add");
+        list1.add(null);
+        list1.clear();
         list1.add(1);
         list1.add(2);
         list1.add(3);
@@ -111,18 +120,18 @@ public class ListTest
         assertTrue("contains 1", list1.contains(1));
         assertTrue("contains 2", list1.contains(2));
         assertTrue("contains 3", list1.contains(3));
-        assertEqual("index 1", 0, list1.index(1));
-        assertEqual("index 2", 1, list1.index(2));
-        assertEqual("index 3", 2, list1.index(3));
-        assertEqual("get 0", 1, list1.get(0));
-        assertEqual("get 1", 2, list1.get(1));
-        assertEqual("get 2", 3, list1.get(2));
+        assertExpected("index 1", 0, list1.index(1));
+        assertExpected("index 2", 1, list1.index(2));
+        assertExpected("index 3", 2, list1.index(3));
+        assertExpected("get 0", 1, list1.get(0));
+        assertExpected("get 1", 2, list1.get(1));
+        assertExpected("get 2", 3, list1.get(2));
         end();
         
         begin("set");
-        assertEqual("set 0", 1, list1.set(0, 4));
-        assertEqual("set 1", 2, list1.set(1, 5));
-        assertEqual("set 2", 3, list1.set(2, 6));
+        assertExpected("set 0", 1, list1.set(0, 4));
+        assertExpected("set 1", 2, list1.set(1, 5));
+        assertExpected("set 2", 3, list1.set(2, 6));
         try
         {
             list1.set(-1, 7);
@@ -148,27 +157,27 @@ public class ListTest
             fail(IndexOutOfBoundsException.class, e);
         }
         assertFalse("empty", list1.empty());
-        assertEqual("size", 3, list1.size());
+        assertExpected("size", 3, list1.size());
         assertFalse("contains 1", list1.contains(1));
         assertFalse("contains 2", list1.contains(2));
         assertFalse("contains 3", list1.contains(3));
         assertTrue("contains 4", list1.contains(4));
         assertTrue("contains 5", list1.contains(5));
         assertTrue("contains 6", list1.contains(6));
-        assertEqual("index 1", -1, list1.index(1));
-        assertEqual("index 2", -1, list1.index(2));
-        assertEqual("index 3", -1, list1.index(3));
-        assertEqual("index 4", 0, list1.index(4));
-        assertEqual("index 5", 1, list1.index(5));
-        assertEqual("index 6", 2, list1.index(6));
-        assertEqual("get 0", 4, list1.get(0));
-        assertEqual("get 1", 5, list1.get(1));
-        assertEqual("get 2", 6, list1.get(2));
+        assertExpected("index 1", -1, list1.index(1));
+        assertExpected("index 2", -1, list1.index(2));
+        assertExpected("index 3", -1, list1.index(3));
+        assertExpected("index 4", 0, list1.index(4));
+        assertExpected("index 5", 1, list1.index(5));
+        assertExpected("index 6", 2, list1.index(6));
+        assertExpected("get 0", 4, list1.get(0));
+        assertExpected("get 1", 5, list1.get(1));
+        assertExpected("get 2", 6, list1.get(2));
         end();
         
         begin("remove");
-        assertEqual("remove 2", 6, list1.remove(2).intValue());
-        assertEqual("remove 1", 5, list1.remove(1).intValue());
+        assertExpected("remove 2", 6, list1.remove(2));
+        assertExpected("remove 1", 5, list1.remove(1));
         try
         {
             list1.remove(-1);
@@ -181,7 +190,7 @@ public class ListTest
         {
             fail(IndexOutOfBoundsException.class, e);
         }
-        assertEqual("remove 0", 4, list1.remove(0).intValue());
+        assertExpected("remove 0", 4, list1.remove(0));
         try
         {
             list1.remove(0);
@@ -195,22 +204,22 @@ public class ListTest
             fail(IndexOutOfBoundsException.class, e);
         }
         assertTrue("empty", list1.empty());
-        assertEqual("size", 0, list1.size());
+        assertExpected("size", 0, list1.size());
         assertFalse("contains 4", list1.contains(4));
         assertFalse("contains 5", list1.contains(5));
         assertFalse("contains 6", list1.contains(6));
-        assertEqual("index 4", -1, list1.index(4));
-        assertEqual("index 5", -1, list1.index(5));
-        assertEqual("index 6", -1, list1.index(6));
+        assertExpected("index 4", -1, list1.index(4));
+        assertExpected("index 5", -1, list1.index(5));
+        assertExpected("index 6", -1, list1.index(6));
         end();
         
         for (int i = 0; i < 50; ++i)
         {
             begin("many add one " + i);
             list1.add(i);
-            assertEqual("size", i + 1, list1.size());
-            assertEqual("get", i, list1.get(i));
-            assertEqual("index", i, list1.index(i));
+            assertExpected("size", i + 1, list1.size());
+            assertExpected("get", i, list1.get(i));
+            assertExpected("index", i, list1.index(i));
             assertTrue("contains", list1.contains(i));
             assertFalse("empty", list1.empty());
             end();
@@ -222,9 +231,9 @@ public class ListTest
             int getValue = list1.get(i).intValue();
             int removeValue = list1.remove(i).intValue();
             assertEqual("remove == get", getValue, removeValue);
-            assertEqual("size", i, list1.size());
-            assertEqual("value", i, removeValue);
-            assertEqual("index", -1, list1.index(removeValue));
+            assertExpected("size", i, list1.size());
+            assertExpected("value", i, removeValue);
+            assertExpected("index", -1, list1.index(removeValue));
             assertFalse("contains", list1.contains(removeValue));
             if (i > 0)
             {
@@ -240,17 +249,17 @@ public class ListTest
         begin("equals");
         list1.clear();
         list2.clear();
-        assertTrue("zero zero", list1.equals(list2));
+        assertEqual("zero zero", list1, list2);
         list1.add(1);
-        assertFalse("one zero", list1.equals(list2));
+        assertNotEqual("one zero", list1, list2);
         list2.add(1);
-        assertTrue("one one", list1.equals(list2));
+        assertEqual("one one", list1, list2);
         list1.add(2);
-        assertFalse("two one", list1.equals(list2));
+        assertNotEqual("two one", list1, list2);
         list2.add(2);
-        assertTrue("two two", list1.equals(list2));
+        assertEqual("two two", list1, list2);
         list2.set(1, 3);
-        assertFalse("two two not equal", list1.equals(list2));
+        assertNotEqual("two two not equal", list1, list2);
         end();
         
         begin("hashCode");
