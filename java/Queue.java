ce427d7fc781811fae3e0c27f39a7436e546ce86
Will Faught <will.faught@gmail.com>
2012-09-17 17:55:06 -0700

    Formatted. Added queued iterator.

--- java/Queue.java
+++ java/Queue.java
@@ -1,8 +1,14 @@
 package com.willfaught;
 
+import java.util.Iterator;
+
 public interface Queue<E> extends Collection<E>
 {
     E dequeue();
+
     void enqueue(E element);
+
     E head();
+
+    Iterator<E> queued();
 }
