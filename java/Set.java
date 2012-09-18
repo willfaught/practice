62b5e5a2d35052f6b56f6a56eacd743471bb3ac9
Will Faught <will.faught@gmail.com>
2012-09-17 17:57:10 -0700

    Removed stubs. Added TODO.

--- java/Set.java
+++ java/Set.java
@@ -1,15 +1,6 @@
 package com.willfaught;
 
-// TODO
-//public class Set<E> extends Collection<E>
-//{
-//    void add(E element)
-//    {
-//        
-//    }
-//    
-//    void remove(E element)
-//    {
-//        
-//    }
-//}
+public class Set<E>
+{
+    // TODO
+}
