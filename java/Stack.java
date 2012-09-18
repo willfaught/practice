a8e82dfa111d2a3e4cd65c189a2245de49f39c73
Will Faught <will.faught@gmail.com>
2012-09-17 17:55:46 -0700

    Added stacked iterator.

--- java/Stack.java
+++ java/Stack.java
@@ -1,10 +1,14 @@
 package com.willfaught;
 
+import java.util.Iterator;
+
 public interface Stack<E> extends Collection<E>
 {
     E pop();
 
     void push(E element);
+
+    Iterator<E> stacked();
 
     E top();
 }
