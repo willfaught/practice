3d89ec463f4f3e9e49e756a1e82d7f904030b682
Will Faught <will.faught@gmail.com>
2012-09-18 01:52:20 -0700

    Changed all int[] to Integer[]. Added a Comparator and shared instance for Integers. Formatted and sorted members. Renamed sort to test. Renamed all tests to remove the 'Sort' suffix. Reworded some test names. Changed some assertEquals to assertExpecteds.

--- java/ArraySortTest.java
+++ java/ArraySortTest.java
@@ -1,195 +1,220 @@
 package com.willfaught;
 
 import static com.willfaught.Assert.*;
+import java.util.Comparator;
 
 public class ArraySortTest
 {
     private interface ArraySorter
     {
-        void sort(int[] a);
+        void sort(Integer[] a);
     }
-    
-    @Test
-    public void bubbleSort()
+
+    private static class IntegerComparator implements Comparator<Integer>
     {
-        sort(new ArraySorter()
+        public static final IntegerComparator instance = new IntegerComparator();
+
+        @Override
+        public int compare(Integer a, Integer b)
         {
-            @Override
-			public void sort(int[] a)
-            {
-                ArraySort.bubble(a);
-            }
-        });
-    }
-    
-    @Test
-    public void heapSort()
-    {
-        sort(new ArraySorter()
-        {
-            @Override
-			public void sort(int[] a)
-            {
-                ArraySort.heap(a);
-            }
-        });
-    }
-    
-    @Test
-    public void insertionSort()
-    {
-        sort(new ArraySorter()
-        {
-            @Override
-			public void sort(int[] a)
-            {
-                ArraySort.insertion(a);
-            }
-        });
-    }
-    
-    @Test
-    public void mergeBottomUpSort()
-    {
-        sort(new ArraySorter()
-        {
-            @Override
-			public void sort(int[] a)
-            {
-                ArraySort.mergeBottomUp(a);
-            }
-        });
-    }
-    
-    @Test
-    public void mergeTopDownSort()
-    {
-        sort(new ArraySorter()
-        {
-            @Override
-			public void sort(int[] a)
-            {
-                ArraySort.mergeTopDown(a);
-            }
-        });
-    }
-    
-    @Test
-    public void selectionSort()
-    {
-        sort(new ArraySorter()
-        {
-            @Override
-			public void sort(int[] a)
-            {
-                ArraySort.selection(a);
-            }
-        });
-    }
-    
-    private static void sort(ArraySorter arraySorter)
-    {
-        int[] array = new int[0];
-        arraySorter.sort(array);
-        
-        array = new int[] { 1 };
-        arraySorter.sort(array);
-        assertExpected("one", 1, array[0]);
-        
-        array = new int[] { 1, 2 };
-        arraySorter.sort(array);
-        assertExpected("two sorted", new int[] { 1, 2 }, array);
-        
-        array = new int[] { 2, 1 };
-        arraySorter.sort(array);
-        assertExpected("two reversed", new int[] { 1, 2 }, array);
-        
-        array = new int[] { 1, 1 };
-        arraySorter.sort(array);
-        assertExpected("two same", new int[] { 1, 1 }, array);
-        
-        array = new int[] { 1, 2, 3 };
-        arraySorter.sort(array);
-        assertExpected("three sorted", new int[] { 1, 2, 3 }, array);
-        
-        array = new int[] { 3, 2, 1 };
-        arraySorter.sort(array);
-        assertExpected("three reversed", new int[] { 1, 2, 3 }, array);
-        
-        array = new int[] { 1, 1, 1 };
-        arraySorter.sort(array);
-        assertExpected("three same", new int[] { 1, 1, 1 }, array);
-        
-        array = new int[] { 1, 2, 3, 4, 5 };
-        arraySorter.sort(array);
-        assertExpected("five sorted", new int[] { 1, 2, 3, 4, 5 }, array);
-        
-        array = new int[] { 5, 4, 3, 2, 1 };
-        arraySorter.sort(array);
-        assertExpected("five reversed", new int[] { 1, 2, 3, 4, 5 }, array);
-        
-        array = new int[] { 1, 1, 1, 1, 1 };
-        arraySorter.sort(array);
-        assertExpected("five same", new int[] { 1, 1, 1, 1, 1 }, array);
-        
-        array = new int[101];
-        int[] array2 = new int[101];
-        for (int i = 0; i < array.length; ++i)
-        {
-            array[i] = array2[i] = i + 1;
+            return a.compareTo(b);
         }
-        arraySorter.sort(array);
-        assertEqual("odd many sorted", array, array2);
-        
-        for (int i = 0; i < array.length; ++i)
+    }
+
+    private static void test(ArraySorter s)
+    {
+        Integer[] a = new Integer[0];
+        s.sort(a);
+
+        a = new Integer[] { 1 };
+        s.sort(a);
+        assertExpected("one", 1, a[0]);
+
+        a = new Integer[] { 1, 2 };
+        s.sort(a);
+        assertExpected("two asc", new Integer[] { 1, 2 }, a);
+
+        a = new Integer[] { 2, 1 };
+        s.sort(a);
+        assertExpected("two desc", new Integer[] { 1, 2 }, a);
+
+        a = new Integer[] { 1, 1 };
+        s.sort(a);
+        assertExpected("two same", new Integer[] { 1, 1 }, a);
+
+        a = new Integer[] { 1, 2, 3 };
+        s.sort(a);
+        assertExpected("three asc", new Integer[] { 1, 2, 3 }, a);
+
+        a = new Integer[] { 3, 2, 1 };
+        s.sort(a);
+        assertExpected("three desc", new Integer[] { 1, 2, 3 }, a);
+
+        a = new Integer[] { 1, 1, 1 };
+        s.sort(a);
+        assertExpected("three same", new Integer[] { 1, 1, 1 }, a);
+
+        a = new Integer[] { 1, 2, 3, 4, 5 };
+        s.sort(a);
+        assertExpected("five asc", new Integer[] { 1, 2, 3, 4, 5 }, a);
+
+        a = new Integer[] { 5, 4, 3, 2, 1 };
+        s.sort(a);
+        assertExpected("five desc", new Integer[] { 1, 2, 3, 4, 5 }, a);
+
+        a = new Integer[] { 1, 1, 1, 1, 1 };
+        s.sort(a);
+        assertExpected("five same", new Integer[] { 1, 1, 1, 1, 1 }, a);
+
+        a = new Integer[101];
+        Integer[] a2 = new Integer[101];
+        for (int i = 0; i < a.length; ++i)
         {
-            array[i] = array.length - i;
+            a[i] = a2[i] = i + 1;
         }
-        arraySorter.sort(array);
-        assertEqual("odd many reversed", array, array2);
-        
-        array = new int[100];
-        array2 = new int[100];
-        for (int i = 0; i < array.length; ++i)
+        s.sort(a);
+        assertExpected("odd many asc", a2, a);
+
+        for (int i = 0; i < a.length; ++i)
         {
-            array[i] = array2[i] = i + 1;
+            a[i] = a.length - i;
         }
-        arraySorter.sort(array);
-        assertEqual("even many sorted", array, array2);
-        
-        for (int i = 0; i < array.length; ++i)
+        s.sort(a);
+        assertExpected("odd many desc", a2, a);
+
+        a = new Integer[100];
+        a2 = new Integer[100];
+        for (int i = 0; i < a.length; ++i)
         {
-            array[i] = array.length - i;
+            a[i] = a2[i] = i + 1;
         }
-        arraySorter.sort(array);
-        assertEqual("even many reversed", array, array2);
-        
-        array = new int[101];
+        s.sort(a);
+        assertExpected("even many asc", a2, a);
+
+        for (int i = 0; i < a.length; ++i)
+        {
+            a[i] = a.length - i;
+        }
+        s.sort(a);
+        assertExpected("even many desc", a2, a);
+
+        a = new Integer[101];
         boolean sorted = true;
         do
         {
-            for (int i = 0; i < array.length; ++i)
+            for (int i = 0; i < a.length; ++i)
             {
-                array[i] = (int)(Math.random() * Integer.MAX_VALUE);
+                a[i] = (int)(Math.random() * Integer.MAX_VALUE);
             }
-            for (int i = 1; i < array.length; ++i)
+            for (int i = 1; i < a.length; ++i)
             {
-                if (array[i - 1] > array[i])
+                if (a[i - 1] > a[i])
                 {
                     sorted = false;
                 }
             }
         }
         while (sorted);
-        arraySorter.sort(array);
-        for (int i = 1; i < array.length; ++i)
+        s.sort(a);
+        for (int i = 1; i < a.length; ++i)
         {
-            boolean condition = array[i - 1] <= array[i];
-            assertTrue("random random: indices=" + (i - 1) + ", " + i + "; values=" + array[i - 1] + ", " + array[i], condition);
+            boolean condition = a[i - 1] <= a[i];
+            assertTrue("random random: indices=" + (i - 1) + ", " + i + "; values=" + a[i - 1] + ", " + a[i], condition);
             if (!condition)
             {
                 break;
             }
         }
+    }
+
+    @Test
+    public void bubble()
+    {
+        test(new ArraySorter()
+        {
+            @Override
+            public void sort(Integer[] a)
+            {
+                ArraySort.bubble(a, IntegerComparator.instance);
+            }
+        });
+    }
+
+    @Test
+    public void heap()
+    {
+        test(new ArraySorter()
+        {
+            @Override
+            public void sort(Integer[] a)
+            {
+                ArraySort.heap(a, IntegerComparator.instance);
+            }
+        });
+    }
+
+    @Test
+    public void insertion()
+    {
+        test(new ArraySorter()
+        {
+            @Override
+            public void sort(Integer[] a)
+            {
+                ArraySort.insertion(a, IntegerComparator.instance);
+            }
+        });
+    }
+
+    @Test
+    public void mergeBottomUp()
+    {
+        test(new ArraySorter()
+        {
+            @Override
+            public void sort(Integer[] a)
+            {
+                ArraySort.mergeBottomUp(a, IntegerComparator.instance);
+            }
+        });
+    }
+
+    @Test
+    public void mergeTopDown()
+    {
+        test(new ArraySorter()
+        {
+            @Override
+            public void sort(Integer[] a)
+            {
+                ArraySort.mergeTopDown(a, IntegerComparator.instance);
+            }
+        });
+    }
+
+    @Test
+    public void quick()
+    {
+        test(new ArraySorter()
+        {
+            @Override
+            public void sort(Integer[] a)
+            {
+                ArraySort.quick(a, IntegerComparator.instance);
+            }
+        });
+    }
+
+    @Test
+    public void selection()
+    {
+        test(new ArraySorter()
+        {
+            @Override
+            public void sort(Integer[] a)
+            {
+                ArraySort.selection(a, IntegerComparator.instance);
+            }
+        });
     }
 }
