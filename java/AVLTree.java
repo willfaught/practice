116658c0cae44ac80a86340a837d3586ff2e1b79
Will Faught <will.faught@gmail.com>
2012-09-20 19:36:59 -0700

    Initial commit.

---
+++ java/AVLTree.java
@@ -0,0 +1,28 @@
+package com.willfaught;
+
+public class AVLTree<K extends Comparable<K>, V> extends BinarySearchTree<K, V>
+{
+    @Override
+    public V add(K key, V value)
+    {
+        return null; // TODO
+    }
+
+    @Override
+    public V remove(K key)
+    {
+        return null; // TODO
+    }
+
+    @Override
+    public void removeMaximum()
+    {
+        // TODO
+    }
+
+    @Override
+    public void removeMinimum()
+    {
+        // TODO
+    }
+}
