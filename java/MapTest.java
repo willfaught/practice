f60929562f620c7f63ed181eaef5d0ac6eeac9aa
Will Faught <will.faught@gmail.com>
2012-10-10 18:51:28 -0700

    Added spacing. Factored out a magic number. Added additional checks for the add many cases. Added TernarySearchTree.

--- java/MapTest.java
+++ java/MapTest.java
@@ -44,106 +44,129 @@ public class MapTest
         begin("one");
 
         begin("add");
+
         assertExpected("add", null, m.add(1, 2));
         assertTrue("contains", m.contains(1));
         assertFalse("empty", m.empty());
         assertExpected("size", 1, m.size());
         assertExpected("get", 2, m.get(1));
 
+        end();
+
         begin("replace");
+
         assertExpected("add", 2, m.add(1, 3));
         assertTrue("contains", m.contains(1));
         assertFalse("empty", m.empty());
         assertExpected("size", 1, m.size());
         assertExpected("get", 3, m.get(1));
+
         end();
 
         begin("remove");
+
         assertExpected("remove", 3, m.remove(1));
         assertFalse("contains", m.contains(1));
         assertTrue("empty", m.empty());
         assertExpected("size", 0, m.size());
         assertExpected("get", null, m.get(1));
+
         end();
 
         end();
 
         begin("add many");
+
+        int size = 1000;
         m.clear();
-        for (int i = 1; i <= 1000; ++i)
+        for (int i = 1; i <= size; ++i)
         {
             begin("add " + i + "," + (i + 1));
+
             assertExpected("add", null, m.add(i, i + 1));
             assertTrue("contains", m.contains(i));
             assertFalse("empty", m.empty());
             assertExpected("size", i, m.size());
             assertExpected("get", i + 1, m.get(i));
+
             end();
 
             begin("replace " + i + "," + (i + 2));
+
             assertExpected("add", i + 1, m.add(i, i + 2));
             assertTrue("contains", m.contains(i));
             assertFalse("empty", m.empty());
             assertExpected("size", i, m.size());
             assertExpected("get", i + 2, m.get(i));
+
             end();
 
             begin("remove " + i + "," + (i + 2));
+
             assertExpected("remove", i + 2, m.remove(i));
             assertFalse("contains", m.contains(i));
             assertEqual("empty", i == 1, m.empty());
             assertExpected("size", i - 1, m.size());
             assertExpected("get", null, m.get(i));
+
             end();
 
             begin("add again " + i + "," + (i + 3));
+
             assertExpected("add", null, m.add(i, i + 3));
             assertTrue("contains", m.contains(i));
             assertFalse("empty", m.empty());
             assertExpected("size", i, m.size());
             assertExpected("get", i + 3, m.get(i));
+
             end();
         }
+
         end();
 
-        for (int i = 1; i <= 1000; ++i)
+        for (int i = 1; i <= size; ++i)
         {
             begin("check many " + i + "," + (i + 3));
+
             assertTrue("contains", m.contains(i));
             assertExpected("get", i + 3, m.get(i));
+
             end();
         }
 
-        for (int i = 1; i <= 1000; ++i)
+        for (int i = 1; i <= size; ++i)
         {
             begin("remove many asc " + i + "," + (i + 3));
+
             assertExpected("remove", i + 3, m.remove(i));
             assertFalse("contains", m.contains(i));
-            assertEqual("empty", i == 1000, m.empty());
-            assertExpected("size", 1000 - i, m.size());
+            assertEqual("empty", i == size, m.empty());
+            assertExpected("size", size - i, m.size());
             assertExpected("get", null, m.get(i));
+
             end();
         }
 
-        for (int i = 1; i <= 1000; ++i)
+        for (int i = 1; i <= size; ++i)
         {
             m.add(i, i + 3);
         }
 
-        for (int i = 1000; i >= 1; --i)
+        for (int i = size; i >= 1; --i)
         {
             begin("remove many desc " + i + "," + (i + 3));
+
             assertExpected("remove", i + 3, m.remove(i));
             assertFalse("contains", m.contains(i));
             assertEqual("empty", i == 1, m.empty());
             assertExpected("size", i - 1, m.size());
             assertExpected("get", null, m.get(i));
+
             end();
         }
 
-        end();
-
         begin("add one remove many");
+
         m.clear();
         m.add(1, 2);
         for (int i = 1; i <= 50; ++i)
@@ -154,6 +177,7 @@ public class MapTest
         assertFalse("empty", m.empty());
         assertExpected("size", 1, m.size());
         assertExpected("get", 2, m.get(1));
+
         end();
     }
 
@@ -197,116 +221,180 @@ public class MapTest
         begin("one");
 
         begin("add");
+
         assertExpected("add", null, m.add("1", 2));
         assertTrue("contains", m.contains("1"));
         assertFalse("empty", m.empty());
         assertExpected("size", 1, m.size());
         assertExpected("get", 2, m.get("1"));
 
+        end();
+
         begin("replace");
+
         assertExpected("add", 2, m.add("1", 3));
         assertTrue("contains", m.contains("1"));
         assertFalse("empty", m.empty());
         assertExpected("size", 1, m.size());
         assertExpected("get", 3, m.get("1"));
+
         end();
 
         begin("remove");
+
         assertExpected("remove", 3, m.remove("1"));
         assertFalse("contains", m.contains("1"));
         assertTrue("empty", m.empty());
         assertExpected("size", 0, m.size());
         assertExpected("get", null, m.get("1"));
+
         end();
 
         end();
 
         begin("add many");
+
+        int size = 100;
         m.clear();
-        for (int i = 1; i <= 1000; ++i)
+        for (int i = 1; i <= size; ++i)
         {
             begin("add " + i + "," + (i + 1));
+
             assertExpected("add", null, m.add("" + i, i + 1));
-            assertTrue("contains", m.contains("" + i));
             assertFalse("empty", m.empty());
             assertExpected("size", i, m.size());
+            assertTrue("contains", m.contains("" + i));
             assertExpected("get", i + 1, m.get("" + i));
+            
+            for (int j = 1; j < i; ++j)
+            {
+                begin("check " + j + "," + (j + 3));
+                
+                assertTrue("contains", m.contains("" + j));
+                assertExpected("get", j + 3, m.get("" + j));
+                
+                end();
+            }
+
             end();
 
             begin("replace " + i + "," + (i + 2));
+            
             assertExpected("add", i + 1, m.add("" + i, i + 2));
-            assertTrue("contains", m.contains("" + i));
             assertFalse("empty", m.empty());
             assertExpected("size", i, m.size());
+            assertTrue("contains", m.contains("" + i));
             assertExpected("get", i + 2, m.get("" + i));
+
+            for (int j = 1; j < i; ++j)
+            {
+                begin("check " + j + "," + (j + 3));
+                
+                assertTrue("contains", m.contains("" + j));
+                assertExpected("get", j + 3, m.get("" + j));
+                
+                end();
+            }
+            
             end();
 
             begin("remove " + i + "," + (i + 2));
+
             assertExpected("remove", i + 2, m.remove("" + i));
-            assertFalse("contains", m.contains("" + i));
             assertEqual("empty", i == 1, m.empty());
             assertExpected("size", i - 1, m.size());
+            assertFalse("contains", m.contains("" + i));
             assertExpected("get", null, m.get("" + i));
+            
+            for (int j = 1; j < i; ++j)
+            {
+                begin("check " + j + "," + (j + 3));
+                
+                assertTrue("contains", m.contains("" + j));
+                assertExpected("get", j + 3, m.get("" + j));
+                
+                end();
+            }
+
             end();
 
             begin("add again " + i + "," + (i + 3));
+
             assertExpected("add", null, m.add("" + i, i + 3));
-            assertTrue("contains", m.contains("" + i));
             assertFalse("empty", m.empty());
             assertExpected("size", i, m.size());
-            assertExpected("get", i + 3, m.get("" + i));
-            end();
-        }
-        end();
-
-        for (int i = 1; i <= 1000; ++i)
-        {
-            begin("check many " + i + "," + (i + 3));
             assertTrue("contains", m.contains("" + i));
             assertExpected("get", i + 3, m.get("" + i));
+            
+            for (int j = 1; j < i; ++j)
+            {
+                begin("check " + j + "," + (j + 3));
+                
+                assertTrue("contains", m.contains("" + j));
+                assertExpected("get", j + 3, m.get("" + j));
+                
+                end();
+            }
+
             end();
         }
 
-        for (int i = 1; i <= 1000; ++i)
+        end();
+
+        for (int i = 1; i <= size; ++i)
+        {
+            begin("check many " + i + "," + (i + 3));
+
+            assertTrue("contains", m.contains("" + i));
+            assertExpected("get", i + 3, m.get("" + i));
+
+            end();
+        }
+
+        for (int i = 1; i <= size; ++i)
         {
             begin("remove many asc " + i + "," + (i + 3));
+
             assertExpected("remove", i + 3, m.remove("" + i));
+            assertEqual("empty", i == size, m.empty());
+            assertExpected("size", size - i, m.size());
             assertFalse("contains", m.contains("" + i));
-            assertEqual("empty", i == 1000, m.empty());
-            assertExpected("size", 1000 - i, m.size());
             assertExpected("get", null, m.get("" + i));
+
             end();
         }
 
-        for (int i = 1; i <= 1000; ++i)
+        for (int i = 1; i <= size; ++i)
         {
             m.add("" + i, i + 3);
         }
 
-        for (int i = 1000; i >= 1; --i)
+        for (int i = size; i >= 1; --i)
         {
             begin("remove many desc " + i + "," + (i + 3));
+
             assertExpected("remove", i + 3, m.remove("" + i));
-            assertFalse("contains", m.contains("" + i));
             assertEqual("empty", i == 1, m.empty());
             assertExpected("size", i - 1, m.size());
+            assertFalse("contains", m.contains("" + i));
             assertExpected("get", null, m.get("" + i));
+
             end();
         }
 
-        end();
-
         begin("add one remove many");
+
         m.clear();
         m.add("" + 1, 2);
-        for (int i = 1; i <= 50; ++i)
+        for (int i = 2; i <= 10; ++i)
         {
-            m.remove("" + 2);
+            m.remove("" + i);
         }
         assertTrue("contains", m.contains("" + 1));
         assertFalse("empty", m.empty());
         assertExpected("size", 1, m.size());
         assertExpected("get", 2, m.get("" + 1));
+
         end();
     }
 
@@ -328,6 +416,12 @@ public class MapTest
     {
         testInteger(new SeparateChainingHashTable<Integer, Integer>());
         testInteger(new SeparateChainingHashTable<Integer, Integer>(10));
+    }
+
+    @Test
+    public void ternarySearchTree()
+    {
+        testString(new TernarySearchTree<Integer>());
     }
 
     @Test
